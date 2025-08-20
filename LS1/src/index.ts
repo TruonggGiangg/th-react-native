c66e80ac24bc691e9bb7ec363a75de57206c7a8e
TruonggGiangg <truonggiang2299z@gmail.com>
2025-08-20 07:35:54 +0700

    lab1-1

--- LS1/src/index.ts
+++ LS1/src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world 1');
+class Student {
+    private name: string;
+    private age: number;
+
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    toString(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
+
+const student: Student = new Student("John Doe", 20);
+
+console.log("LS1 - 1");
+console.log(student.toString());
+
